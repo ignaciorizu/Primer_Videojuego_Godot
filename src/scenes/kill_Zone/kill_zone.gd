c737ba07bce1d20a0933604f35d88eaff21dfe4e
Nacho <Ignacio.rizu@gmail.com>
2026-08-22 17:11:23 -0300

    escena de reinicio y su activacion

--- src/scenes/kill_Zone/kill_zone.gd
+++ src/scenes/kill_Zone/kill_zone.gd
@@ -13,4 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("jugador"):
-		get_tree().change_scene_to_file("res://src/scenes/world/win.tscn")
+		get_tree().reload_current_scene()
