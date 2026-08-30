ec5e5728e7611e16af76578deb2d4a01f90dc863
Nacho <Ignacio.rizu@gmail.com>
2026-08-30 00:21:12 -0300

    Vidas del personaje

--- src/scenes/world/world.gd
+++ src/scenes/world/world.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var current_checkpoint_position: Vector2 = Vector2.ZERO
+const MAX_LIVES: int = 3
+var current_lives: int = MAX_LIVES
+
+signal lives_changed(new_lives_count: int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,3 +22,14 @@ func get_respawn_position() -> Vector2:
 		push_warning("Advertencia: No hay checkpoint registrado. Retornando origen.")
 		
 	return current_checkpoint_position
+	
+func subtract_life() -> void:
+	current_lives -= 1
+	lives_changed.emit(current_lives)
+	
+	if current_lives <= 0:
+		_reset_game_state()
+
+func _reset_game_state() -> void:
+	current_lives = MAX_LIVES
+	current_checkpoint_position = Vector2.ZERO
