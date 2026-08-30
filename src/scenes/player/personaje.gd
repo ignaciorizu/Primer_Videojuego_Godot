b361418b6e1ce1c8d2fb8d5c316f8394a7cbde3d
Nacho <Ignacio.rizu@gmail.com>
2026-08-30 14:26:46 -0300

    refactor de codigo del personaje e implementacion de flip horizontal al caminar

--- src/scenes/player/personaje.gd
+++ src/scenes/player/personaje.gd
@@ -1,41 +1,46 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
-@onready var _animation_player = $AnimationPlayer
+const SPEED: float = 300.0
+const JUMP_VELOCITY: float = -500.0
+
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _sprite: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	_initialize_spawn_position()
 
-func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		_animation_player.play("walk")
-	elif Input.is_action_pressed("ui_left"):
-		_animation_player.play("walk")
-	else:
-		_animation_player.stop()
+func _physics_process(delta: float) -> void:
+	_apply_gravity(delta)
+	_handle_jump()
+	
+	var direction: float = Input.get_axis("ui_left", "ui_right")
+	_apply_movement(direction)
+	_update_visual_state(direction)
+	
+	move_and_slide()
 
 func _initialize_spawn_position() -> void:
 	var respawn_pos: Vector2 = world.get_respawn_position()
-	
 	if respawn_pos != Vector2.ZERO:
 		global_position = respawn_pos
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+func _handle_jump() -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+func _apply_movement(direction: float) -> void:
+	if direction != 0.0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func _update_visual_state(direction: float) -> void:
+	if direction != 0.0:
+		_sprite.flip_h = direction > 0.0
+		_animation_player.play("walk")
+	else:
+		_animation_player.stop()
