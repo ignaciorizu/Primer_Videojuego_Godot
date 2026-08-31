e770c65de3b63df2c2946da92c36cb220126f5c8
Nacho <Ignacio.rizu@gmail.com>
2026-08-31 01:08:01 -0300

    Boton de reinicio implementado y funaciona con click o espacio o intro

--- src/scenes/world/reset_button.gd
+++ src/scenes/world/reset_button.gd
@@ -1,11 +1,29 @@
-extends CenterContainer
+extends Control
 
+const INITIAL_LEVEL_PATH: String = "res://src/scenes/world/world.tscn"
+const INPUT_ACTION_ACCEPT: String = "ui_accept"
 
-# Called when the node enters the scene tree for the first time.
+@onready var _button_reference: Button = $BaseButton
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_button_reference.pressed.connect(_execute_game_restart)
+	_validate_setup()
 
+func _input(event: InputEvent) -> void:
+	_evaluate_keyboard_input(event)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _evaluate_keyboard_input(event: InputEvent) -> void:
+	if event.is_action_pressed(INPUT_ACTION_ACCEPT):
+		get_viewport().set_input_as_handled()
+		_execute_game_restart()
+
+func _validate_setup() -> void:
+	if _button_reference == null:
+		push_error("Error critico: Referencia nula para BaseButton.")
+		return
+	if INITIAL_LEVEL_PATH == "":
+		push_error("Error critico: Ruta de nivel inicial vacia.")
+		return
+
+func _execute_game_restart() -> void:
+	get_tree().change_scene_to_file.call_deferred(INITIAL_LEVEL_PATH)
